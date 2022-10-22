e53334db73a8baefb0c6c7f41bf5f492002c443d
rongftruong <76272514+GiongfNef@users.noreply.github.com>
2022-10-22 16:44:56 +0700

    test chall

--- static/js/background.js
+++ static/js/background.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     particleground(document.getElementById('particles'), {
-      dotColor: '#4B0082',
-      lineColor: '#4B0082'
+      dotColor: '#5cbdaa',
+      lineColor: '#5cbdaa'
     });
     var intro = document.getElementById('intro');
     intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
